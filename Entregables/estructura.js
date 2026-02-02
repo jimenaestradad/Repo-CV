0413bfa6ec6983509ab14178fdbbe9b4be86be4a
Jimena Estrada <mjestradad@ufm.edu>
2026-02-02 10:49:37 -0600

    Dynamic Greeting

---
+++ Entregables/estructura.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const greeting = document.getElementById("greeting");
+  const hour = new Date().getHours();
+
+  if (hour < 12) {
+    greeting.textContent = "Buenos días / Good morning!";
+  } else if (hour < 18) {
+    greeting.textContent = "Buenas tardes / Good afternoon!";
+  } else {
+    greeting.textContent = "Buenas noches / Good evening!";
+  }
+});
+
